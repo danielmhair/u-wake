135a91c37478e45e2b87a9a5826e33b530d25e28
Daniel Hair <danielmhair@gmail.com>
2023-02-28 04:10:05 +0000

    fix: update login

--- app/apps/app/src/app/logged-out/login/login.component.ts
+++ app/apps/app/src/app/logged-out/login/login.component.ts
@@ -1,5 +1,6 @@
 import { SocialAuthService } from '@abacritt/angularx-social-login';
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostBinding, Inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 
@@ -10,19 +11,35 @@ import { Subject, takeUntil } from 'rxjs';
   encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor(private socialAuth: SocialAuthService, private router: Router) {}
+  constructor(private socialAuth: SocialAuthService, private router: Router, @Inject(DOCUMENT) public document: Document) { }
   private destroy$ = new Subject()
+  private observer: MutationObserver = null
+
+  @HostBinding('class.has-credential-container')
+  public googleContainerShowing = false
 
   public ngOnDestroy() {
     this.destroy$.next(false)
     this.destroy$.complete()
+    this.observer.disconnect()
   }
 
   public async ngOnInit() {
     this.socialAuth.authState.pipe(takeUntil(this.destroy$)).subscribe(user => {
       if (user) {
         this.router.navigate(['/'])
+        return
       }
+    })
+
+    this.googleContainerShowing = !!this.document.querySelector('#credential_picker_container')
+    this.observer = new MutationObserver(mutations => {
+      this.googleContainerShowing = !!this.document.querySelector('#credential_picker_container');
+    });
+
+    this.observer.observe(this.document.documentElement, {
+      childList: true,
+      subtree: true,
     })
   }
 }
